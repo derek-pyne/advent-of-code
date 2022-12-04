d910984390190238e92c37b2b71ded8dcd27a963
Derek Pyne <derek.pyne@gmail.com>
2022-12-04 13:49:18 -0500

    Formatting 2020 day 1

--- src/year_2020/day1.py
+++ src/year_2020/day1.py
@@ -1,11 +1,6 @@
-f_path = '2020/inputs/day1.txt'
+from typing import List
 
-nums = []
-with open(f_path) as f:
-    for line in f:
-        nums.append(int(line))
-
-target = 2020
+from src.advent_of_code_puzzle import AdventOfCodePuzzle
 
 
 def get_sum_nums(nums, target):
@@ -17,13 +12,27 @@ def get_sum_nums(nums, target):
     return None, None
 
 
-num, desired = get_sum_nums(nums, target=2020)
-print(f'Num 1:{num} num 2: {desired} multiply {num * desired}')
+def get_sum_nums(nums, target):
+    for num in nums:
+        desired = target - num
+        if desired in nums:
+            return num, desired
 
-for num in nums:
-    other_nums = nums.copy()
-    other_nums.remove(num)
-    num2, num3 = get_sum_nums(other_nums, target=2020 - num)
-    if num2 is not None:
-        print(f'num: {num}, num2: {num2}, num3: {num3}, multiple: {num * num2 * num3}')
-        break
+    return None, None
+
+
+class Day1(AdventOfCodePuzzle):
+    def part_1(self, inputs: List[str]) -> int:
+        nums = [int(input) for input in inputs]
+
+        num, desired = get_sum_nums(nums, target=2020)
+        return num * desired
+
+    def part_2(self, inputs: List[str]) -> int:
+        nums = [int(input) for input in inputs]
+        for num in nums:
+            other_nums = nums.copy()
+            other_nums.remove(num)
+            num2, num3 = get_sum_nums(other_nums, target=2020 - num)
+            if num2 is not None:
+                return num * num2 * num3
